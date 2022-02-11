1151f84f10794ac3c2dea9f9dc34887962697e23
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-02-11 16:41:39 -0300

    Fazendo a primeira migração pro banco de dados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    preco = models.DecimalField('preco', max_digits=7, decimal_places=2)
+    estoque = models.IntegerField('qtd_em_estoque')
--- core/views.py
+++ core/views.py
@@ -3,8 +3,21 @@ from django.views.generic import TemplateView
 
 
 class Index(TemplateView):
-     template_name = 'index.html'
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = {'ola': 'ola mundo'}
+        return context
+
 
 
 class Contact(TemplateView):
     template_name = 'contact.html'
+
+
+# class Contact(ListView):
+#     template_name = 'contact.html'
+#
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         return qs
