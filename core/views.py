a7557b4951e29af3267cdb0c27aa3b2754bb490e
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-02-11 14:00:21 -0300

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+     template_name = 'index.html'
+
+
+class Contact(TemplateView):
+    template_name = 'contact.html'
